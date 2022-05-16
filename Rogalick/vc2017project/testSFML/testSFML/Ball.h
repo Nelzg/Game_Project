502185e7ff31ea41f01c0b79e2aab9a0c753cf43
Girka-Kirill <90462891+Girka-Kirill@users.noreply.github.com>
2022-05-16 11:49:50 +0300

    Update Ball.h

--- Rogalick/vc2017project/testSFML/testSFML/Ball.h
+++ Rogalick/vc2017project/testSFML/testSFML/Ball.h
@@ -7,13 +7,14 @@ class Ball : public MaterialPoint
 public:
 	float radius;
 	int type;
+	int damage;
 	sf::CircleShape circle;
 	sf::Texture texture;
 
 	Ball(int radius_) {
 		radius = radius_;
 		circle.setRadius(radius);
-		texture.loadFromFile("D:/Game_Project/Rogalick/bin/fireball.png");
+		texture.loadFromFile("fireball.png");
 		circle.setTexture(&texture);
 		circle.setOrigin(radius, radius);
 	}
